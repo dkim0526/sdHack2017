d05becd8b7256d5600b3dc9e77d69bb8af9d94f3
Daniel Kim <bjkim0526@gmail.com>
2016-09-30 21:05:48 -0700

    delleted jade and added handlerbars

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var handlebars = require("express-handlebars");
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +13,10 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine("html", handlebars());
+app.set("view engine", "html"); 
+app.set("views", __dirname + "/views");
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
